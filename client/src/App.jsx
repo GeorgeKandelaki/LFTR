ab8102c1f3b31d26ae97535c64f6e47e1d9f293e
GeorgeKandelaki <kandelakig29@hotmail.com>
2026-02-03 22:28:52 +0400

    adding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import { BrowserRouter, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { Toaster } from "react-hot-toast";
+
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
     return (
         <>
+            <GlobalStyles />
             <Toaster position="top-center" reverseOrder={false} />
             <BrowserRouter>
-                <Routes></Routes>
+                <Routes>
+                    <Route index element={<h1>HOME PAGE</h1>} />
+
+                    <Route path="/login" />
+                    <Route path="/signup" />
+
+                    <Route path="/profile" />
+                    <Route path="/settings" />
+                    <Route path="/dashboard" />
+                    <Route path="/history" />
+                    <Route path="/workout" />
+                </Routes>
             </BrowserRouter>
         </>
     );
